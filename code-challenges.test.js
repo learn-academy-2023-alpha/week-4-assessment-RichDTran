f0a30b75bd27a7e35f93022340c6db41b57d8353
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-03-15 09:50:44 -0700

    Updated Assesment will go over with mentor Thursday for coding questions

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -22,40 +22,40 @@ const { array } = require("yargs")
 
 //**UPDATED PSEUDOCODE: Welcome back, after confiding in my cowrokers/peers they recommended an easier method called .shift() which removed the first element in the array and explained the expect.arrayContaining(array) belongs into my test and not function as well to create NOT to equal expect.  Thank you for reading, see you on the next pseudocode!
 
-// const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-// const colors2 = [
-//   "chartreuse",
-//   "indigo",
-//   "periwinkle",
-//   "ochre",
-//   "aquamarine",
-//   "saffron"
-// ]
-// const shuffle = (array) => {
-//   array.shift()
-//     return array
+const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+const colors2 = [
+  "chartreuse",
+  "indigo",
+  "periwinkle",
+  "ochre",
+  "aquamarine",
+  "saffron"
+]
+const shuffle = (array) => {
+  array.shift()
+    return array
   
-  // }
+  }
 
 
 
-// describe("shuffle", () => {
-//   it("removes the first item from the array and shuffles the remaining content", () => {
-//     expect(shuffle(colors1)).toEqual(expect.arrayContaining(["blue", "green", "yellow", "pink"]))
-//     expect(shuffle(colors1)).not.toEqual(expect.arrayContaining(["purple"]))
-//     expect(shuffle(colors2)).toEqual(expect.arrayContaining([
-//       "indigo",
-//       "periwinkle",
-//       "ochre",
-//       "aquamarine",
-//       "saffron"
-//     ]))
-//     expect(shuffle(colors2)).not.toEqual(expect.arrayContaining([
-//       "chartreuse"
-//     ]))
-//   })
-// })
+describe("shuffle", () => {
+  it("removes the first item from the array and shuffles the remaining content", () => {
+    expect(shuffle(colors1)).toEqual(expect.arrayContaining(["blue", "green", "yellow", "pink"]))
+    expect(shuffle(colors1)).not.toEqual(expect.arrayContaining(["purple"]))
+    expect(shuffle(colors2)).toEqual(expect.arrayContaining([
+      "indigo",
+      "periwinkle",
+      "ochre",
+      "aquamarine",
+      "saffron"
+    ]))
+    expect(shuffle(colors2)).not.toEqual(expect.arrayContaining([
+      "chartreuse"
+    ]))
+  })
+})
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 // b) Create the function that makes the test pass.
 
@@ -64,6 +64,10 @@ const { array } = require("yargs")
 //PSEUDOCODE: 
 //Welcome back to some more Pseudocode I hope you enjoyed the first one! In this major event we will be creating a function called voteTracker. voteTracker will take in sets of numbers in the form of upVotes and downVotes parameters where the function will poerform upVotes-downVotes=tallied votes.
 
+const voteTracker = (upVotes, downVotes) => {
+  return upVotes - downVotes
+}
+*******This is the original with various different tries when it wasnt working so there are more but this was the original. I thought it was straight forward but was wondering why isnt this working.
 
 //**PSEUDOCODE UPDATE: Well i wish i could say i did this myself but i didnt :( What was missing for me is that i CANNOT access the object contents like upvotes and downvotes WITHOUT going through the object its self. */
 // a) Create a test with expect statements for each of the variables provided.
@@ -90,23 +94,23 @@ const { array } = require("yargs")
 
 // a) Create a test with an expect statement using the variables provided.
 
-const dataArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+// const dataArray1 = ["array", "object", "number", "string", "Boolean"]
+// const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
 // b) Create the function that makes the test pass.
-const arrayMerge= (array1, array2) => {
-  const newArray = array1.splice(3, 1, []).concat(array2.splice(0, 1, []))
-  return arrayMerge()
-  }
+// const arrayMerge= (array1, array2) => {
+//   const newArray = array1.splice(3, 1, []).concat(array2.splice(0, 1, []))
+//   return arrayMerge()
+//   }
 
-  console.log(arrayMerge(dataArray1, dataArray2))
+//   console.log(arrayMerge(dataArray1, dataArray2))
 
 
-describe("arrayMerge", () => {
-  it("returns one array with no duplicate values.", () => {
-    expect(arrayMerge(votes1)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
-  })
-})
+// describe("arrayMerge", () => {
+//   it("returns one array with no duplicate values.", () => {
+//     expect(arrayMerge(votes1)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+//   })
+// })
 
 //** I tried removing the "duplicates by using splice while concatting the arrays but need further research of what to do next." */
--- interview-questions.md
+++ interview-questions.md
@@ -10,7 +10,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Your answer:Object-oriented programming is when coding is bases its foundations on coding around the object to make fuctional code. Such as creating a a method and bringing in object to perform in that method. Whereas fuctional programming is the process of producing code to have a specific behavior or perform a certain task. An example is creating a function and passing through arguments or data to perform the behavior or task it has been created for, like removing odd numbers in array where the array is the data inputted into a function and produces only even numbers.
 
-Researched answer:
+Researched answer: Object Oriented Programming are classes and objects. It is structured for simple reusable code. This is a good style of coding because it is can be dynamically used such as coding for movies: run time, title, year made. This code would be able to produce over all movies rather than being restricted to one. The four building blocks are Classes, Objects, Methods and Attributes.
 
 1. What is the difference between a Float and an Integer in Ruby?
 
@@ -45,11 +45,12 @@ Researched answer: Hash is a data structure used to store data in the form of Un
 ## Looking Ahead: Terms for Next Week
 
 1. Class Inheritance:
-
+Class inheritance is when  another class retrieves a behaivor from another class. One will be the parent class and the other a child class. Parent class would become "superclass" which will be the main classwhere all the child classes will retrieve the original behaivor.
 2. RSpec:
-
+Rspec is a testing tool for Ruby. Very similar to javascript's yarn jest.
 3. CRUD:
-
+Stands for Create, Read, Update, and Delete which are storage applications to keep data even when system goes down. 
 4. Test-driven development:
-
+Test-driven development coincies with Rspec. We as developers will test how coding works if it will pass or not.
 5. HTTP:
+An easy to use client library in Ruby requests.
